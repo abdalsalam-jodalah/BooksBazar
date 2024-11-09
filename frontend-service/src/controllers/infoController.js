22a666469ebd2a8abf66cdeb01555bc96e0de143
saadeh <ahmadmalek169@yahoo.com>
2024-11-09 20:57:58 +0200

    update bug

--- frontend-service/src/controllers/infoController.js
+++ frontend-service/src/controllers/infoController.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const {getCache} = require("../cacheHelper");
+const { getCache, setCache } = require("../cacheHelper");
 
 const info = async (req, res) => {
     try {
